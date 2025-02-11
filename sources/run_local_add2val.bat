6dc21a4503fb425a0dcdebe398498ddf788d0e84
DidierDefrance <didier@d-defrance.fr>
2025-02-11 09:03:17 +0100

    initial

---
+++ sources/run_local_add2val.bat
@@ -0,0 +1,9 @@
+REM python add2vals.py 5 6
+REM The result is 11
+REM python add2vals.py jean_ Bon
+REM The result is jean_Bon
+python add2vals.py 5.2 6.3
+REM The result is 11.5
+
+REM python test_calc.py
+pause
